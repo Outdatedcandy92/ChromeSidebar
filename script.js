c1b993ed2befd68b7cc1c1ba05d20a61c15b17a5
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-07-08 23:08:13 -0400

    Settings work

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-import { API_KEY } from './config.js';
+let userName;
+let APIKEYValue;
+let defaultPromptValue;
+let modelTypeValue;
+
 import { GoogleGenerativeAI } from "./gemini.js";
+
+const config = localStorage.getItem('config');
+
+
+console.log(config);
+if (config) {
+  // Parse it into an object
+  const Obj = JSON.parse(config);
+
+  // Access the name property
+  const name = Obj.name;
+  const APIKEY = Obj.APIKEY;
+  const defaultPrompt = Obj.prompt;
+  const modelType = Obj.modelType;
+
+  
+
+
+  if (name) {
+    console.log(name); 
+    userName = name;
+  } else {
+    console.log('Name is empty.');
+    userName = 'You';
+  }
+  if (APIKEY) {
+    console.log(APIKEY);
+    APIKEYValue = APIKEY;
+  } else {
+    console.log('APIKEY is empty.');
+
+  }
+  if (defaultPrompt) {
+    console.log(defaultPrompt);
+    defaultPromptValue = defaultPrompt;
+  } else {
+    console.log('Default prompt is empty.');
+    defaultPromptValue = 'Hello, You are my study assistant.';
+  }
+
+  if (modelType) {
+    console.log(modelType);
+    modelTypeValue = modelType;
+  } else {
+    console.log('Model type is empty.');
+    modelTypeValue = 'gemini-1.5-flash';
+  }
+} else {
+  console.log('No config found in localStorage.');
+}
+
 
 function scrollToBottom() {
     const responseaiElement = document.getElementById('responseai');
@@ -7,17 +62,17 @@ function scrollToBottom() {
   }
 
 // Access your API key (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(API_KEY);
+const genAI = new GoogleGenerativeAI(APIKEYValue);
 
 async function run() {
 // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({ model: modelTypeValue});
 
 const chat = model.startChat({
     history: [
     {
         role: "user",
-        parts: [{ text: "Hello, You are my study assistant." }],
+        parts: [{ text: defaultPromptValue }],
     },
     {
         role: "model",
@@ -57,7 +112,7 @@ document.getElementById('promptInput').addEventListener('keypress', function(eve
     if (event.key === 'Enter') {
       event.preventDefault(); // Prevent the default action to stop form submission or any other unwanted behavior
       sendPrompt(); // Call the sendPrompt function
-      document.getElementById('responseai').innerText += `You:\n ${document.getElementById('promptInput').value}\n`;
+      document.getElementById('responseai').innerText += `${userName}\n ${document.getElementById('promptInput').value}\n`;
       document.getElementById('promptInput').value = ''; // Clear the input field using direct reference
     }
   });
